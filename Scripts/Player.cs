f9baf2f172c8547a4cae34baf7ad17418bf37d3a
tamurkhan <t.khan7@wlv.ac.uk>
2019-01-21 19:32:09 +0000

    Freeze player movement when chat open

---
+++ Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class Player : MonoBehaviour
+{
+    //reference for thridperson player controller
+    [SerializeField] private ThirdPersonUserControl _control;
+
+    private void Start()
+    {
+        gamemanager.Instance.OpenedChat += OnOpenedChat;
+    }
+
+    private void OnOpenedChat()
+    {
+        _control.ISFreeze = true;
+    }
+}
